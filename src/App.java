a3b39c9f8d6a8ba0bc70801e5761116558e3720b
J44v11 <javiergarcialopez101@gmail.com>
2026-05-29 13:06:01 +0100

    feat: Agrega instancias de Matematico, Biologo y Quimico en la clase App

--- src/App.java
+++ src/App.java
@@ -1,5 +1,9 @@
 import java.util.Scanner;
 
+import net.salesianos.investigadores.Biologo;
+import net.salesianos.investigadores.Matematico;
+import net.salesianos.investigadores.Quimico;
+
 public class App {
     public static void main(String[] args) throws Exception {
 
@@ -10,5 +14,9 @@ public class App {
                 { 4, 5, 6 },
                 { 7, 8, 9 }
         };
+
+        Matematico matematico = new Matematico("Alan", "Matrices", 1850, matrizInicial);
+        Biologo biologo = new Biologo("Rosalind", "Genetica", 1475);
+        Quimico quimico = new Quimico("Marie", "Reacciones", 1340);
     }
 }
