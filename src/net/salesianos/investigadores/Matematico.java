04be3cbed5a7e99641cd6d2193543df4580b867a
J44v11 <javiergarcialopez101@gmail.com>
2026-05-29 12:37:50 +0100

    feat: Agrego método getResultadosActuales en la clase Matematico

--- src/net/salesianos/investigadores/Matematico.java
+++ src/net/salesianos/investigadores/Matematico.java
@@ -1,11 +1,16 @@
 package net.salesianos.investigadores;
 
 public class Matematico extends Investigador {
-    
+
     protected int[][] resultadosActuales;
 
     public Matematico(String nombre, String especialidad, double sueldo, int[][] resultadosActuales) {
         super(nombre, especialidad, sueldo);
         this.resultadosActuales = resultadosActuales;
     }
+
+    public int[][] getResultadosActuales() {
+        return resultadosActuales;
+    }
+
 }
