af970594748264aa69f07021c0649a2a804db9f7
J44v11 <javiergarcialopez101@gmail.com>
2026-05-29 12:10:17 +0100

    feat: Cambio clase Investigador a abstracta y agrego atributos y constructor

--- src/net/salesianos/investigadores/Investigador.java
+++ src/net/salesianos/investigadores/Investigador.java
@@ -1,5 +1,15 @@
 package net.salesianos.investigadores;
 
-public class Investigador {
-    
+public abstract class Investigador {
+
+    protected String nombre;
+    protected String especialidad;
+    protected double sueldo;
+
+    public Investigador(String nombre, String especialidad, double sueldo) {
+        this.nombre = nombre;
+        this.especialidad = especialidad;
+        this.sueldo = sueldo;
+    }
+
 }
