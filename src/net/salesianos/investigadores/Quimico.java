83840f0ccf3feef2c4e00b8ab7deee69131768a4
J44v11 <javiergarcialopez101@gmail.com>
2026-05-29 13:00:22 +0100

    feat: Implementa la interfaz TrabajoDual en la clase Quimico

--- src/net/salesianos/investigadores/Quimico.java
+++ src/net/salesianos/investigadores/Quimico.java
@@ -1,8 +1,9 @@
 package net.salesianos.investigadores;
 
 import java.util.ArrayList;
+import net.salesianos.trabajodual.TrabajoDual;
 
-public class Quimico extends Investigador {
+public class Quimico extends Investigador implements TrabajoDual {
 
     protected ArrayList<String> elementosReaccion;
 
