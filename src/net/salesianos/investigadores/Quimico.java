b4a9f3571ad597941ff7e3a1657c638696f82666
J44v11 <javiergarcialopez101@gmail.com>
2026-05-29 12:52:29 +0100

    feat: Agrego la clase Quimico que extiende de Investigador

---
+++ src/net/salesianos/investigadores/Quimico.java
@@ -0,0 +1,14 @@
+package net.salesianos.investigadores;
+
+import java.util.ArrayList;
+
+public class Quimico extends Investigador {
+
+    protected ArrayList<String> elementosReaccion;
+
+    public Quimico(String nombre, String especialidad, double sueldo) {
+        super(nombre, especialidad, sueldo);
+        this.elementosReaccion = new ArrayList<>();
+    }
+    
+}
