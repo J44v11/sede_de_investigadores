41d41f9f6252d6039ee2c25486b74d160743050a
J44v11 <javiergarcialopez101@gmail.com>
2026-05-29 12:11:53 +0100

    feat: Agrego método identificacion para mostrar datos del investigador

--- src/net/salesianos/investigadores/Investigador.java
+++ src/net/salesianos/investigadores/Investigador.java
@@ -36,4 +36,10 @@ public abstract class Investigador {
         this.sueldo = sueldo;
     }
 
+    public void identificacion() {
+        System.out.println("--- Datos del investigador ---");
+        System.out.println("Nombre: " + nombre);
+        System.out.println("Especialidad: " + especialidad);
+        System.out.println("Sueldo: " + sueldo);
+    }
 }
