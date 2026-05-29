85f5fa27c54145bc397f5a4f4446d4c31fe2ce9e
J44v11 <javiergarcialopez101@gmail.com>
2026-05-29 12:44:52 +0100

    feat: Agrego método setEspecimenesInvestigacion en la clase Biologo

--- src/net/salesianos/investigadores/Biologo.java
+++ src/net/salesianos/investigadores/Biologo.java
@@ -4,14 +4,18 @@ import java.util.ArrayList;
 
 public class Biologo extends Investigador {
 
-     protected ArrayList<String> especimenesInvestigacion;
+    protected ArrayList<String> especimenesInvestigacion;
 
     public Biologo(String nombre, String especialidad, double sueldo) {
         super(nombre, especialidad, sueldo);
         this.especimenesInvestigacion = new ArrayList<>();
     }
-    
-     public ArrayList<String> getEspecimenesInvestigacion() {
+
+    public ArrayList<String> getEspecimenesInvestigacion() {
         return especimenesInvestigacion;
+    }
+
+    public void setEspecimenesInvestigacion(ArrayList<String> especimenesInvestigacion) {
+        this.especimenesInvestigacion = especimenesInvestigacion;
     }
 }
