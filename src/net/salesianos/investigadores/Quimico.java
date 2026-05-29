24f7f1f6147dc4514e5390b8e2d24de059d1757b
J44v11 <javiergarcialopez101@gmail.com>
2026-05-29 12:55:27 +0100

    feat: Agrego métodos invertirLista y mostrarElementos en la clase Quimico para gestionar elementos de reacción

--- src/net/salesianos/investigadores/Quimico.java
+++ src/net/salesianos/investigadores/Quimico.java
@@ -10,7 +10,7 @@ public class Quimico extends Investigador {
         super(nombre, especialidad, sueldo);
         this.elementosReaccion = new ArrayList<>();
     }
-    
+
     public ArrayList<String> getElementosReaccion() {
         return elementosReaccion;
     }
@@ -34,6 +34,34 @@ public class Quimico extends Investigador {
             return "El quimico necesita solicitar un aumento.";
         } else {
             return "El sueldo del quimico es correcto.";
+        }
+    }
+
+    @Override
+    public void trabajar() {
+        invertirLista();
+        System.out.println("El quimico mezcla los elementos invirtiendo sus posiciones.");
+        mostrarElementos();
+    }
+
+    public void invertirLista() {
+        for (int i = 0; i < elementosReaccion.size() / 2; i++) {
+            String temporal = elementosReaccion.get(i);
+            int ultimaPosicion = elementosReaccion.size() - 1 - i;
+            elementosReaccion.set(i, elementosReaccion.get(ultimaPosicion));
+            elementosReaccion.set(ultimaPosicion, temporal);
+        }
+    }
+
+    public void mostrarElementos() {
+        if (elementosReaccion.size() == 0) {
+            System.out.println("La lista de elementos esta vacia.");
+        } else {
+            System.out.println("--- Elementos de la reaccion ---");
+
+            for (int i = 0; i < elementosReaccion.size(); i++) {
+                System.out.println((i + 1) + ". " + elementosReaccion.get(i));
+            }
         }
     }
 }
